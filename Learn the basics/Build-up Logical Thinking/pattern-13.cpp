3f2b7b887e127d482073875c5f92c5fa4d23ea8f
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-03-28 22:28:17 +0530

    Create pattern-13.cpp

---
+++ Learn the basics/Build-up Logical Thinking/pattern-13.cpp
@@ -0,0 +1,39 @@
+Pattern 13:-  https://practice.geeksforgeeks.org/problems/triangle-pattern-1661718712/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_13
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        int e=1;
+        for(int i=0;i<n;i++){
+           for(int j=0;j<i+1;j++){
+               cout<<e<<" ";
+               e++;
+           }
+           cout<<endl;
+       }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
