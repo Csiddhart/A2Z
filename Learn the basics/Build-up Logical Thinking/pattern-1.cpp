52ccbcf31cba71479ad042ddb2f0146d440f6f3c
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-03-28 21:08:52 +0530

    Create pattern-1.cpp

---
+++ Learn the basics/Build-up Logical Thinking/pattern-1.cpp
@@ -0,0 +1,14 @@
+Pattern 1  :- https://practice.geeksforgeeks.org/problems/square-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_1
+
+
+class Solution {
+  public:
+    void printSquare(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<"*"<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
