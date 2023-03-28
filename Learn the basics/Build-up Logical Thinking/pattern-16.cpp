da01bdc1204f71b16b224bfcd1c95eacef7e07ba
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-03-28 21:54:09 +0530

    Create pattern-16.cpp

---
+++ Learn the basics/Build-up Logical Thinking/pattern-16.cpp
@@ -0,0 +1,37 @@
+Pattern 16:- https://practice.geeksforgeeks.org/problems/triangle-pattern-1662285334/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_16
+  
+  
+  //{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+       char character='A';
+        for(int i=0;i<n;i++){
+           for(int j=0;j<i+1;j++){
+               cout<<char(character+i);
+           }
+           cout<<endl;
+       }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
