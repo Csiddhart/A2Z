39f7efb31945d6050d65327ac9e7add472af7b0a
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-03-28 21:13:33 +0530

    Create pattern-4.cpp

---
+++ Learn the basics/Build-up Logical Thinking/pattern-4.cpp
@@ -0,0 +1,14 @@
+Pattern 4 :- https://practice.geeksforgeeks.org/problems/triangle-number-1661428795/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_4
+
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+    for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                cout<<i+1<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
