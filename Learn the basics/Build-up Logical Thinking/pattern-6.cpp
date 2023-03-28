c9908badfdf4d9470e88b0b6b9d739511f8ee026
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-03-28 21:16:28 +0530

    Create pattern-6.cpp

---
+++ Learn the basics/Build-up Logical Thinking/pattern-6.cpp
@@ -0,0 +1,38 @@
+Pattern 6:- https://practice.geeksforgeeks.org/problems/triangle-number-1661489840/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_6
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	 for(int i=0;i<n;i++){ //FOR CHECKING AND PRINTING ROWS
+	     for(int j=0;j<(n-i);j++){   //FOR CHECKING AND PRINTING COLUMNS 
+	         cout<<j+1<<" ";
+	     }
+	     cout<<endl;
+	 }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
