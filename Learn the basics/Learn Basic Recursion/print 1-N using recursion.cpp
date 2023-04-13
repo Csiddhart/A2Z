feaadc52e2a36f5fcc7565c04eece7fe4c5fd5db
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-04-13 13:15:16 +0530

    Create print 1-N using recursion.cpp

---
+++ Learn the basics/Learn Basic Recursion/print 1-N using recursion.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    void printNos(int N)
+    {
+        //base case
+        
+        if(N==0){
+            return;
+        }
+        
+        // recursive relation 
+        
+        printNos(N-1);
+        
+        //processing 
+        
+        cout<<N<<" ";     // ekhane kono variable a giye store hocche nah so direct print korachi
+        
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main()
+{
+    int T;
+    
+    //taking testcases
+    cin>>T;
+    
+    while(T--)
+    {
+        int N;
+        
+        //input N
+        cin>>N;
+        Solution ob;
+        //calling printNos() function
+        ob.printNos(N);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
